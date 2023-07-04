6a3cfd17037e0c93c3d6342000294f9b257474a1
niken157 <nikencahyaning157@gmail.com>
2023-07-04 10:11:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:statefull_dan_stateless/appPrima.dart';
 import 'package:statefull_dan_stateless/appQuiz.dart';
-import 'package:statefull_dan_stateless/appStatefull.dart';
-import 'package:statefull_dan_stateless/appStateless.dart';
 
 void main(List<String> args) {
   runApp(const AppQuiz());
